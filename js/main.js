b3d511057d8095ad9edd356f9deb7a66279ebbd6
Ajit <68845280+AjitLibrary@users.noreply.github.com>
2024-02-16 16:55:05 +0530

    new

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const searchBar = document.getElementById('search-bar');
 
 // Replace this with your actual publication data
 const publications = [
-  { title: 'Publication 1', description: 'This is a description of publication 1.' },
-  { title: 'Publication 2', description: 'This is a description of publication 2.' },
-  { title: 'Publication 3', description: 'This is a description of publication 3.' },
+  { title: 'Managing scientific research publishing using Open Journal Systems', description: 'This is a description of publication 1.' },
+  { title: 'Library As Publisher: A Successful Story of Indian Institute of Advanced Study Library, Shimla, description: 'This is a description of publication 2.' },
+  { title: 'Bibliometric Mapping of Gender Disparity in Research Publications in India during 1999-2018: A Case Study of Shimla District, Himachal Pradesh', description: 'This is a description of publication 3.' },
 ];
 
 function renderPublications(filteredPublications) {
